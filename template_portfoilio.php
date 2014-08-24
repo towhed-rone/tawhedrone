4c83d330a7e541385da2e6f3096420f51d3fd47d
unknown <Towhed@.(none)>
2014-08-24 10:43:17 +0300

    Successfully implemented

--- template_portfoilio.php
+++ template_portfoilio.php
@@ -7,67 +7,14 @@
 
 <?php get_header();?>
 		
-<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/multizoom.css" type="text/css" />
-
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>
-
-<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/multizoom.js">
-
-// Featured Image Zoomer (w/ optional multizoom and adjustable power)- By Dynamic Drive DHTML code library (www.dynamicdrive.com)
-// Multi-Zoom code (c)2012 John Davenport Scheuer
-// as first seen in http://www.dynamicdrive.com/forums/
-// username: jscheuer1 - This Notice Must Remain for Legal Use
-// Visit Dynamic Drive at http://www.dynamicdrive.com/ for this script and 100s more
-
-</script>
-
-<script type="text/javascript">
-
-jQuery(document).ready(function($){
-
-	$('.attachment-40 .wp-post-image').addimagezoom({ // single image zoom
-		zoomrange: [3, 10],
-		magnifiersize: [300,300],
-		magnifierpos: 'right',
-		cursorshade: true,
-		largeimage: '<?php the_post_thumbnail(); ?>' //<-- No comma after last option!
-	})
-
-
-	$('#image2').addimagezoom({ // single image zoom
-		zoomrange: [3, 10],
-		magnifiersize: [300,300],
-		magnifierpos: 'right',
-		cursorshade: true,
-		largeimage: '<?php the_post_thumbnail(); ?>' //<-- No comma after last option!
-	})
-	
-	$('#multizoom1').addimagezoom({ // multi-zoom: options same as for previous Featured Image Zoomer's addimagezoom unless noted as '- new'
-		descArea: '#description', // description selector (optional - but required if descriptions are used) - new
-		speed: 1500, // duration of fade in for new zoomable images (in milliseconds, optional) - new
-		descpos: true, // if set to true - description position follows image position at a set distance, defaults to false (optional) - new
-		imagevertcenter: true, // zoomable image centers vertically in its container (optional) - new
-		magvertcenter: true, // magnified area centers vertically in relation to the zoomable image (optional) - new
-		zoomrange: [3, 10],
-		magnifiersize: [250,250],
-		magnifierpos: 'right',
-		cursorshadecolor: '#fdffd5',
-		cursorshade: true //<-- No comma after last option!
-	});
-	
-	$('#multizoom2').addimagezoom({ // multi-zoom: options same as for previous Featured Image Zoomer's addimagezoom unless noted as '- new'
-		descArea: '#description2', // description selector (optional - but required if descriptions are used) - new
-		disablewheel: true // even without variable zoom, mousewheel will not shift image position while mouse is over image (optional) - new
-				//^-- No comma after last option!	
-	});
-	
-})
-
-</script>
-	
+<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/prototype.js"></script>
+<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/scriptaculous.js?load=effects"></script>
+<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/lightbox.js"></script>
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/lightbox.css" type="text/css" media="screen" />
 					<div class="main_content fix">
 			<div class="page_content floatleft">
 				<div class="single_portfolio">
+					
 				<?php if(!is_paged()) { ?>
 						<?php
 							$args = array( 'post_type' => 'portfolio-images', 'posts_per_page' => -1 );
@@ -75,8 +22,7 @@ jQuery(document).ready(function($){
 						?>  
 						<?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
 
-
-						<a href="<?php $key="link"; echo get_post_meta($post->ID, $key, true); ?>"> <?php the_post_thumbnail($post->ID,'portfolio-mashpy'); ?></a>
+<a href="<?php $src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array( 760,350 ), false, '' ); echo $src[0]; ?>" rel="lightbox[roadtrip]"><?php the_post_thumbnail('portfolio-thumbnail'); ?></a>
 						
 						<?php endwhile; ?>
 						<?php wp_reset_query(); ?>
