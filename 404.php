fb80484514c050ad48e655637ca77c47ba06c34e
unknown <Towhed@.(none)>
2014-08-25 15:34:22 +0300

    404 page create

---
+++ 404.php
@@ -0,0 +1,16 @@
+<?php get_header();?>
+		
+		
+			
+					<div class="main_content fix">
+			<div class="page_content floatleft">
+<div class="page_not_found">
+	<p><span>404</span>ERROR</p>
+</div>
+			
+			</div>
+			<?php get_sidebar();?>
+			
+			
+		</div>
+			<?php get_footer();?>
